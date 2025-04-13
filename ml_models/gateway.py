a053cb5569fcb81d706b9b899b650c41a00c09ab
Konstantin <email.email.com>
2025-04-13 19:18:27 +0200

    Intermediate commit

--- ml_models/gateway.py
+++ ml_models/gateway.py
@@ -2,24 +2,12 @@ import requests
 
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway, Histogram, pushadd_to_gateway
 
+from constants import PUSH_GATEWAY_URL
 # # Create a Gauge metric
 # gauge = Gauge('example_metric', 'Example metric pushed to Push Gateway', labelnames=['label_name'], registry=registry)
 
-
-
-# # Set the metric value
-# gauge.labels(label_name='example_label').set(42)
-
-# # Push the metric to the Push Gateway
-# # Use pushadd_to_gateway if you want to add or replace metrics with the same name.
-# push_to_gateway(push_gateway_url, job='example_job', registry=registry)
-
-
 def push_gateway_observations(observations):
     # observations = [0, 0, 0, 0, 1.0]
-
-    # Define the Prometheus Push Gateway URL
-    push_gateway_url = 'localhost:9091'
 
     # Create a CollectorRegistry
     registry = CollectorRegistry()
@@ -32,19 +20,12 @@ def push_gateway_observations(observations):
         histogram.observe(value)
 
     # Push the histogram to the Push Gateway
-    push_to_gateway(push_gateway_url, job='training_job', registry=registry)
-
-    # out = {'churn' : 123, 'no_churn': 172}
-
-    # requests.post('http://localhost:9091/metrics/job/metricfire/instance/article', data= observations.json())
+    push_to_gateway(PUSH_GATEWAY_URL, job='training_job', registry=registry)
 
 def push_gateway_numerical_feature_training(buckets, counts):
     """
     buckets:
     """
-
-    # Define the Prometheus Push Gateway URL
-    push_gateway_url = 'localhost:9091'
 
     # Create a CollectorRegistry
     registry = CollectorRegistry()
@@ -56,5 +37,5 @@ def push_gateway_numerical_feature_training(buckets, counts):
         histogram.observe(i)
 
     # Push the histogram to the Push Gateway
-    push_to_gateway(push_gateway_url, job='drift_detection_job', registry=registry)
+    push_to_gateway(PUSH_GATEWAY_URL, job='drift_detection_job', registry=registry)
 
--- ml_models/simulate_feedback.py
+++ ml_models/simulate_feedback.py
@@ -7,13 +7,8 @@ import time
 
 from loguru import logger
 import requests
-
-# from monitoring.train import load_data, parse_pandas_dtypes
 from train import load_data, parse_pandas_dtypes
-
-
-SLEEP_SECONDS = 1
-
+from constants import SLEEP_SECONDS
 
 def simulate_feedback():
 
@@ -27,14 +22,15 @@ def simulate_feedback():
     )
 
     logger.info(f"Simulating feedback for {len(df)} instances")
-    logger.info(f"Sleeping delay for {SLEEP_SECONDS} seconds")
-    time.sleep(SLEEP_SECONDS)
+
 
     for idx, row in df.iterrows():
         data = row.to_json()
         r = requests.post("http://localhost:8000/feedback", data=data)
         logger.info(f"instance: {idx}, feedback: {r.json()}")
-        time.sleep(0.1)
+
+        logger.info(f"Sleeping delay for {SLEEP_SECONDS} seconds")
+        time.sleep(SLEEP_SECONDS)
 
 
 if __name__ == "__main__":
